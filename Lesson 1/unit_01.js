00d82e70e79ab40f684a9545eb485db5af83159d
naughtymor <znaknikita.it@gmail.com>
2022-09-09 01:33:58 +0300

    4

--- Lesson 1/unit_01.js
+++ Lesson 1/unit_01.js
@@ -69,28 +69,53 @@ console.log(a);
 // <img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">
 //Обратите внимание. Вам придется вставить строку, которая уже содержит кавычки. Главное чтобы кавычки чередовались. Поэтому при вставке оберните данную строку одинарными кавычками.
 
+let div7 = document.querySelector('.seven');
+div7.innerHTML = '<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">';
 
 // Task 14.
 //Создайте две переменных z1 = 6, z2 = 3. Создайте div, класс присвойте самостоятельно и выведите в него умножение z1 на z2.
 
+let z1 = 6;
+let z2 = 3;
+document.querySelector('.out-14').innerHTML = z1 * z2;
+
 // Task 15.
 // Создайте две переменных y1 = 6, y2 = 3. Создайте div, класс присвойте самостоятельно, выведите в него деление y1 на y2.
+
+let y1 = 6;
+let y2 = 3;
+document.querySelector('.out-15').innerHTML = y1 / y2;
 
 // Task 16.
 // Создайте две переменные x1='Hello', x2 = 5. Создайте div, класс присвойте самостоятельно, выведите в него сумму x1 + x2. Изучите результат операции.
 
-
+let x1 = 'Hello';
+let x2 = 5;
+document.querySelector('.out-16').innerHTML = x1 + ' ' + x2;
 
 // Task 17.
 // Создайте div с классом test-1. Получите его в переменную d1. Выведите эту переменную в консоль. Изучите вывод.
 
+let d1 = document.querySelector('.test-1');
+console.log(d1);
+
 // Task 18.
 // Создайте div.test-2. Получите его в переменную d2. Выведите эту переменную в консоль. Присвойте d2 значение 5 (d2 = 5). Выведите переменную в консоль. Изучите вывод.
 
-
+let d2 = document.querySelector('.test-2');
+console.log(d2);
+d2 = 5;
+console.log(d2);
 
 // Task 19.
 // Создайте div c классом s3 и div с классом s4. Получите div.s3 в переменную divS3. Выведите в консоль divS3. Теперь получите в эту же переменную divS3 блок div c классом s4. Выведите переменную divS3 в консоль. Изучите что изменилось.
 
+let divS3 = document.querySelector('.s3');
+console.log(divS3);
+divS3 = document.querySelector('.s4');
+console.log(divS3);
+
 // Task 20
 // Получите c помощью querySelector тег body. С помощью innerHTML присвойте ему пустую строку (...innerHTML = ''). Изучите что произошло. Закомментируйте этот код.
+
+//document.querySelector('body').innerHTML = '';
