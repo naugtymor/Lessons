025d9c1874a16f39893f6f2ce2d6c7f892fff041
naughtymor <znaknikita.it@gmail.com>
2022-09-19 11:47:20 +0300

    unit 13 task 1-5 changes

--- Lesson 13/unit_13.js
+++ Lesson 13/unit_13.js
@@ -9,7 +9,7 @@ function f1() {
         "five": 20
     };
     document.querySelector('.out-1').innerHTML = a1.two;
-    //return ...
+    return a1.two;
 }
 
 document.querySelector('.b-1').onclick = f1;
@@ -26,8 +26,8 @@ function f2() {
         "two": "mahai",
         "five": "hi"
     };
-
-    // return
+    document.querySelector('.out-2').innerHTML = a2.five;
+    return a2.five;
 }
 
 document.querySelector('.b-2').onclick = () => {
@@ -49,7 +49,8 @@ function f3() {
         "odd": "hi",
         "mix": "mix"
     };
-    // return
+    document.querySelector('.out-3').innerHTML = a3.five + ' ' + a3.odd;
+    return a3.five + ' ' + a3.odd;
 }
 
 document.querySelector('.b-3').onclick = () => {
@@ -71,8 +72,11 @@ let a4 = {
     "mix": "mix"
 };
 function f4() {
-
-    // return out;
+    let out = '';
+    for (let key in a4) {
+        out += key + ' ' + a4[key] + ' <br>';
+    }
+    return out;
 }
 
 document.querySelector('.b-4').onclick = () => {
@@ -85,10 +89,11 @@ document.querySelector('.b-4').onclick = () => {
 
 function f5(arr, block) {
     let out = '';
-    // цикл
-    // формат вывода `${key} : ${arr[key]} <br>`;
-    //
-    // тут вывод в блок block
+
+    for (let key in arr) {
+        out += `${key} : ${arr[key]} <br>`;
+    }
+    document.querySelector(block).innerHTML = out;
 }
 
 // давайте протестируем f5
