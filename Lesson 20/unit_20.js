1a3f5f450e1761044b619930ae9176fd3e99a646
naughtymor <znaknikita.it@gmail.com>
2022-09-29 02:11:58 +0300

    unit 20 task 1-6 changes

--- Lesson 20/unit_20.js
+++ Lesson 20/unit_20.js
@@ -2,17 +2,27 @@
 // Task 1 ============================================
 /* Дан input .i-1. Напишите функцию t1, которая выводит в .out-1 символ и возвращает его. Во всех последующих задачах - работаем с латиницей и цифрами.*/
 
-function t1() {
+let out1 = document.querySelector('.out-1');
+
+function t1(event) {
+    out1.innerHTML = event.key;
+    return false;
 }
+
+document.querySelector('.i-1').onkeypress = t1;
 
 // ваше событие здесь!!!
 
 // Task 2 ============================================
 /*  Дан input .i-2. Напишите функцию t2, которая выводит в .out-2 код символа и возвращает его. */
 
-function t2() {
+let out2 = document.querySelector('.out-2');
 
+function t2(event) {
+    out2.innerHTML = event.keyCode;
 }
+
+document.querySelector('.i-2').onkeypress = t2;
 
 // ваше событие здесь!!!
 
@@ -20,11 +30,20 @@ function t2() {
 // Task 3 ============================================
 /*  Дан input .i-3. Напишите функцию t3, которая выводит на страницу true если введен символ и false если цифра. Для определения - используйте код клавиши. */
 
-let w3 = 75;
+let out3 = document.querySelector('.out-3');
 
-function t3() {
-
+function t3(event) {
+    df
+    if (event.keyCode >= 48 && event.keyCode <= 57) {
+        out3.innerHTML = false;
+    }
+    else {
+        out3.innerHTML = true;
+    }
 }
+
+document.querySelector('.i-3').onkeypress = t3;
+
 
 // ваше событие здесь!!!
 
@@ -32,27 +51,55 @@ function t3() {
 // Task 4 ============================================
 /*  Дан input .i-4. Напишите функцию t4, которая выводит в .out-4 только символы в нижнем регистре. Т.е. ввели ab4Bci в out получаем ab4bci. */
 
-function t4() {
+let out4 = document.querySelector('.out-4');
 
+function t4(event) {
+    if ((event.charCode >= 97 && event.charCode <= 122) || (event.charCode >= 48 && event.charCode <= 57)) {
+        out4.textContent += event.key;
+    } else if (event.charCode >= 65 && event.charCode <= 90) {
+        out4.textContent += event.key.toLowerCase();
+    }
 }
+
+
+document.querySelector('.i-4').onkeypress = t4;
 
 // ваше событие здесь!!!
 
 // Task 5 ============================================
 /*  Дан input .i-5. Напишите функцию t5, которая выводит в .out-5 все вводимые символы в верхнем регистре. Т.е. пользователь ввел AbCd и функция выведет ABCD. */
 
-function t5() {
+let out5 = document.querySelector('.out-5');
+
+function t5(event) {
+    if ((event.charCode >= 97 && event.charCode <= 122) || (event.charCode >= 48 && event.charCode <= 57)) {
+        out5.textContent += event.key.toUpperCase();
+    } else if (event.charCode >= 65 && event.charCode <= 90) {
+        out5.textContent += event.key;
+    }
 
 }
+
+document.querySelector('.i-5').onkeypress = t5;
 
 // ваше событие здесь!!!
 
 // Task 6 ============================================
 /*  Дан input .i-6. Напишите функцию t6, которая выводит в .i-6 только символы в нижнем регистре.  */
 
-function t6() {
+let i6 = document.querySelector('.i-6');
 
+function t6(event) {
+    if ((event.charCode >= 97 && event.charCode <= 122) || (event.charCode >= 48 && event.charCode <= 57)) {
+        i6.value += event.key;
+    } else if (event.charCode >= 65 && event.charCode <= 90) {
+        i6.value += event.key.toLowerCase();
+    }
+    return false;
 }
+
+document.querySelector('.i-6').onkeypress = t6;
+
 
 // ваше событие здесь!!!
 
